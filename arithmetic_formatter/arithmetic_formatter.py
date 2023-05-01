c31f562e82e5b8bd6a126c1f9a26e2df57b492a2
holdcarter <holdcarter@holdcarter.ru>
2023-05-01 09:24:23 +0300

    finish refactoring

--- arithmetic_formatter/arithmetic_formatter.py
+++ arithmetic_formatter/arithmetic_formatter.py
@@ -1,38 +1,5 @@
 import operator
 
-# def first_line(problems: list[str]):
-#     res = ''
-#     for element in problems:
-#         slot = length_of_expression(element)
-#         res += f'{element.split()[0].rjust(slot)}{free_area()}'
-
-#     return res[:-4]
-
-
-# def second_line(problems: list[str]):
-#     res = ''
-#     for element in problems:
-#         slot = length_of_expression(element)
-#         res += f'{element.split()[1]} {element.split()[2].rjust(slot - 2)}{free_area()}'
-    
-#     return res[:-4]
-
-
-# def third_line(problems):
-#     res = ''
-#     for el in problems:
-#         slot = length_of_expression(el)
-#         res += '-' * slot + free_area()
-    
-#     return res[:-4]
-
-
-# def fourth_line(problems):
-#     res = ''
-#     for el in problems:
-#         slot = length_of_expression(el)
-#         res += result_exp(el).rjust(slot) + free_area()
-#     return res[:-4]
 
 def result_expression(expression:str) -> int:
     lst_exp = expression.split()
@@ -48,39 +15,38 @@ def space_bw_problem(separator=' ', space_count=4):
     res = separator * space_count
     return res
 
-def arithmetic_arranger(problems: list[str], need_result=False) -> str:
-    first_line = ''
-    second_line = ''
-    third_line = ''
-    result_line = ''
-    result = ''
-    free_space = space_bw_problem()
+
+def length_of_expression(expression: str):
+    return max(map(len, expression.split())) + 2
+
+
+def arithmetic_arranger(problems: list[str], need_result=True) -> str:
+    first_line = []
+    second_line = []
+    third_line = []
+    result_line = []
+    result = []
 
     for problem in problems:
         slot = length_of_expression(problem)
         operand_1 = problem.split()[0].rjust(slot)
-        operator =  problem.split()[1]
-        operand_2 = problem.split()[2].rjust(slot)
-        first_line += operand_1
-        second_line += f'{operator} {operand_2}'
-        third_line += f"{'-' * slot}"
-        result_line += result_expression(problem).rjust(slot)
-    
-    result = [first_line, second_line, third_line, result_line]
+        operand_2 = f'{problem.split()[1]} {problem.split()[2]}'.rjust(slot)
+        first_line.append(operand_1)
+        second_line.append(f'{operand_2}')
+        third_line.append(f"{'-' * slot}")
+        result_line.append(result_expression(problem).rjust(slot))
+
+
     if need_result:
-        return '\n'.join(result[:-1])
+        lines_combain = [first_line, second_line, third_line, result_line]
+    else:
+        lines_combain = [first_line, second_line, third_line]
+
+    for line in lines_combain:
+            result.append(f'{space_bw_problem()}'.join(line))
+
     return '\n'.join(result)
-
-
-def length_of_expression(expression: str):
-    return len(max(expression.split())) + 2
-
-
-# def formatter(expression):
-#     print(first_line(expression), second_line(expression), third_line(expression), fourth_line(expression), sep='\n')
 
 
 test_case = ['148 + 3033','3536 - 868', '4143 + 23234']
 print(arithmetic_arranger(test_case))
-
-# formatter(test_case)
