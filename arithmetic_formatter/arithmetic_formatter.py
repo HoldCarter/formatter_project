55625d46fdd6d5fa31187fc936e3cb23e87ccf58
holdcarter <holdcarter@holdcarter.ru>
2023-05-01 18:27:08 +0300

    take free_code_camp badge

--- arithmetic_formatter/arithmetic_formatter.py
+++ arithmetic_formatter/arithmetic_formatter.py
@@ -21,6 +21,8 @@ def length_of_expression(expression: str):
 
 
 def arithmetic_arranger(problems: list[str], need_result=True) -> str:
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
     first_line = []
     second_line = []
     third_line = []
@@ -28,14 +30,24 @@ def arithmetic_arranger(problems: list[str], need_result=True) -> str:
     result = []
 
     for problem in problems:
+        
         slot = length_of_expression(problem)
-        operand_1 = problem.split()[0].rjust(slot)
-        operand_2 = f'{problem.split()[1]} {problem.split()[2]}'.rjust(slot)
-        first_line.append(operand_1)
-        second_line.append(f'{operand_2}')
+        operand_1 = problem.split()[0]
+        operator = problem.split()[1]
+        if operator[0] not in ('+', '-'):
+            return "Error: Operator must be '+' or '-'."
+        operand_2 = problem.split()[2]
+
+        if not operand_1.isdigit() or not operand_2.isdigit():
+            return 'Error: Numbers must only contain digits.'
+
+        if len(operand_1) > 4 or len(operand_2) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
+
+        first_line.append(operand_1.rjust(slot))
+        second_line.append(f'{operator.ljust(2)}{operand_2.rjust(slot - 2)}')
         third_line.append(f"{'-' * slot}")
         result_line.append(result_expression(problem).rjust(slot))
-
 
     if need_result:
         lines_combain = [first_line, second_line, third_line, result_line]
@@ -43,10 +55,10 @@ def arithmetic_arranger(problems: list[str], need_result=True) -> str:
         lines_combain = [first_line, second_line, third_line]
 
     for line in lines_combain:
-            result.append(f'{space_bw_problem()}'.join(line))
+        result.append(f'{space_bw_problem()}'.join(line))
 
     return '\n'.join(result)
 
 
-test_case = ['148 + 3033','3536 - 868', '4143 + 23234']
+test_case = ['148 + 3033','3536 - 868', '4143 + 3234']
 print(arithmetic_arranger(test_case))
